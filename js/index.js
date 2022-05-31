dd686aa86196110915bd529b09b3138bfb0f37dc
Erlyn Ascarate <erlyndavid@outlook.es>
2022-05-31 14:52:40 -0500

    Actualiza los inputs con los caracteres permitidos cada vez que el formulario detecte un cambio y el input a enviar solo se envia con los valores permitidos

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", updateTheInputThatWillBeSent);
+function updateTheInputThatWillBeSent() {
+    const countryCode = form.querySelector("#country-code");
+    const telephone = form.querySelector("#telephone");
+
+    const number = `${countryCode.value}${telephone.value}`.replace(/[^0-9]/g, "");
+
+    const phone = form.querySelector("#phone");
+    phone.value = number;
+}
+
+form.addEventListener("change", updateTheInputsWithTheAllowedCharacters);
+function updateTheInputsWithTheAllowedCharacters(event) {
+    let charactersToReplace;
+
+    const input = event.target;
+    switch (input.id) {
+        case "country-code":
+            charactersToReplace = /[^0-9+]/g;
+            break;
+        case "telephone":
+            charactersToReplace = /[^0-9 ()-]/g;
+            break;
+    }
+    const inputValue = input.value.replace(charactersToReplace, "");
+    input.value = inputValue;
+}
